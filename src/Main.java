ee2e13b0a8960e43a18e68d5f3dc592d1d254d1c
Екатерина Бакланова <ekakotelevskaya@yandex.ru>
2022-07-11 22:18:28 +0300

    new file

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int ticketprice = 5000;
-        int mileprice = 20;
-        int bonusmiles = ticketprice / mileprice;
-        System.out.println("Бонусные мили:" + bonusmiles);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
